e7205392d5ed8dccf6e2943dd501e98524fb946b
abdullah201240 <abdullahalsakib7075@gmail.com>
2024-09-05 11:46:41 +0600

    .env

--- src/config.tsx
+++ src/config.tsx
@@ -1,8 +1,8 @@
-export const API_BASE_URL = 'http://localhost:8080/api';
-export const API_BASE_URL_IMAGE = 'http://localhost:8080';
-export const success_url = 'http://localhost:8080/api/user/payment';
-export const fail_url = 'http://localhost:8080/api/user/payment-fail';
-export const cancel_url = 'http://localhost:8080/api/user/payment-cancel';
+export const API_BASE_URL = 'https://interiobackend.onrender.com/api';
+export const API_BASE_URL_IMAGE = 'https://interiobackend.onrender.com';
+export const success_url = 'https://interiobackend.onrender.com/api/user/payment';
+export const fail_url = 'https://interiobackend.onrender.com/api/user/payment-fail';
+export const cancel_url = 'https://interiobackend.onrender.com/api/user/payment-cancel';
 export const sandboxUrl = 'https://secure.aamarpay.com/index.php ';
 export const signature_key = '004abeea2ce47c9e352404f03e1a7167';
 export const store_id = 'interiobd';
